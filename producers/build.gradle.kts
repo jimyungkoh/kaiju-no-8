edfbc6c4f70a885e841163c9cf9613f5ed151428
jimyungkoh <kojimyung1206@naver.com>
2024-09-03 00:17:48 +0900

    chore: producers 모듈 의존 명세
    - slf4j
    - logback
    - kafka-client

---
+++ producers/build.gradle.kts
@@ -0,0 +1,13 @@
+dependencies {
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.1.2")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    testImplementation("org.slf4j:slf4j-simple:2.0.13")
+}
