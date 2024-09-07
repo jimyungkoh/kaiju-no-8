5a94c2ab045014dfd77598e2a89503fce8f28cb8
jimyungkoh <kojimyung1206@naver.com>
2024-09-07 22:13:21 +0900

    chore: 의존성 명세 수정
    - 코루틴 활용을 위한 jvm 버전업 (1.9.20 -> 2.0.0)
    - 랜덤 값 생성을 위한 javafaker 패키지 명세

--- producers/build.gradle.kts
+++ producers/build.gradle.kts
@@ -8,6 +8,12 @@ dependencies {
     // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
     implementation("ch.qos.logback:logback-classic:1.5.6")
 
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+
+    // https://mvnrepository.com/artifact/com.github.javafaker/javafaker
+    implementation("com.github.javafaker:javafaker:1.0.2")
+
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     testImplementation("org.slf4j:slf4j-simple:2.0.13")
 }
